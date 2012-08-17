ab41a1a4d65c33551fa1b67e5cd1b6754b7914cb
Daniel M Ben-David <dannybd@mit.edu>
2012-08-17 00:10:38 -0400

    took CourseRoad offline with 503 page

---
+++ 503.php
@@ -0,0 +1,31 @@
+<?php
+
+if((isset($_GET['access']) and $_GET['access']==md5("guest"))){
+	setcookie('beta', 'notquitesecurebutgoodenough', time()+60*60*24*365);
+	header("Location: /index.php?hash=welcome");
+	die();
+}
+
+header("HTTP/1.1 503 Service Temporarily Unavailable");
+header("Status: 503 Service Temporarily Unavailable");
+header("Retry-After: 86400");
+
+?>
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>CourseRoad is upgrading!</title>
+	<style>
+		body{
+			background-color: #EEE;
+			font-family: Arial;
+		}
+	</style>
+</head>
+<body>
+	<h1>Maintenance time!</h1>
+	<p>Don't worry, CourseRoad will be back shortly, new and improved! Sorry for the inconvenience. (I'll try to be quick!)</p>
+	<p>If something's gone horribly wrong and you need something <em>urgently</em>, or you're perhaps interested in helping me beta-test, email me at <a href="courseroad@mit.edu">courseroad@mit.edu</a>.</p>
+	<p>Thanks,<br>dannybd</p>
+</body>
+</html>
