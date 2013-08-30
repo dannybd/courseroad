ad6ed5433eb63555d2676cdd8c5878701c1260a7
Daniel M Ben-David <dannybd@mit.edu>
2013-08-30 01:17:56 -0400

    Moved majors data to separate file and prettified

--- index.php
+++ index.php
@@ -447,6 +447,7 @@ header('Content-type: text/html; charset=utf-8');
 	<!--[if lt IE 9]><script type="text/javascript" src="/js/excanvas.compiled.js"></script><![endif]-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+	<script src="/js/majors.js<?= $nocache ?>"></script>
 	<script src="/js/cr.js<?= $nocache ?>"></script>
 	<!--script src="/js/d3.js"></script-->
 	<script>
