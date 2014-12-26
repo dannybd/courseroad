68715a8e7e0bcf4fe0e2d7b9652032f901a3c808
Daniel M Ben-David <dannybd@mit.edu>
2014-12-26 01:26:55 -0500

    Move CourseRoadDB out of its lone directory, tidy up settings.ini

--- connect.php
+++ connect.php
@@ -1,12 +1,11 @@
 <?php
 
-require('settings.ini');
-
 if (!__DEV__) {
   error_reporting(0);
 }
 
-require('php/CourseRoadDB.php');
+require('settings.ini');
+require('CourseRoadDB.php');
 CourseRoadDB::initialize(
   $db_URL, $db_username, $db_password, $db_name, $db_salt
 );
