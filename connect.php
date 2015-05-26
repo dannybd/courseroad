a4d8fcdc1c5ac63d2cf33bcfc9a81aae20ddb95a
Daniel M Ben-David <dannybd@mit.edu>
2015-05-26 15:33:50 -0400

    Define baseURL using server vars

--- connect.php
+++ connect.php
@@ -6,6 +6,8 @@ if (!__DEV__) {
   error_reporting(0);
 }
 
+$baseURL = 'https://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['REQUEST_URI']);
+
 require('CourseRoadDB.php');
 CourseRoadDB::initialize(
   $db_URL, $db_username, $db_password, $db_name, $db_salt
