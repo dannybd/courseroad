353cecc94e0f0cc11218ae6f76295fd6c8c806ef
Daniel M Ben-David <dannybd@mit.edu>
2015-05-26 04:10:17 -0400

    Store warehouse creds as a un/pw block instead of just password

--- pull-new-classes.sh
+++ pull-new-classes.sh
@@ -4,7 +4,7 @@ echo "" > $OUTPUT
 DATE=$(date +"%d-%b-%Y" --date="1 days ago")
 env LD_LIBRARY_PATH=~/Downloads/instantclient_11_2 TWO_TASK=warehouse \
 rlwrap -H /tmp/sqlplus_hist ~/Downloads/instantclient_11_2/sqlplus \
--S dannybd/$(cat ~/cron_scripts/.warehousepw) <<EOF
+-S $(cat ~/cron_scripts/.warehousecreds) <<EOF
 set echo on verify off feedback off
 set linesize 10000
 set pagesize 0
