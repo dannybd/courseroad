15f881dccbff20b76c7c7a56ed00af83fbf416e1
Daniel M Ben-David <dannybd@mit.edu>
2015-05-31 17:21:39 -0400

    Add test mode to pull-new-classes bash script, as well as flag to set how many days old the data should/can be

--- pull-new-classes.sh
+++ pull-new-classes.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 OUTPUT=~/cron_scripts/output.html
 echo "" > $OUTPUT
-DATE=$(date +"%d-%b-%Y" --date="1 days ago")
+NUMDAYS=1
+TEST_MODE=""
+while getopts "n:t" opt; do
+  case $opt in
+    n)
+      NUMDAYS=${OPTARG}
+      ;;
+    t)
+      echo "ENTERING TEST MODE"
+      TEST_MODE="?test"
+      ;;
+    \?)
+      echo "Invalid option: -${OPTARG}" >&2
+      exit 1
+      ;;
+  esac
+done
+DATE=$(date +"%d-%b-%Y" --date="${NUMDAYS} days ago")
+echo "Looking for classes from up to ${NUMDAYS} days ago: ${DATE}"
 env LD_LIBRARY_PATH=~/Downloads/instantclient_11_2 TWO_TASK=warehouse \
 rlwrap -H /tmp/sqlplus_hist ~/Downloads/instantclient_11_2/sqlplus \
 -S $(cat ~/cron_scripts/.warehousecreds) <<EOF
@@ -10,8 +28,8 @@ set linesize 10000
 set pagesize 0
 set markup html on
 spool $OUTPUT
-SELECT * FROM cis_course_catalog WHERE last_activity_date='$DATE';
+SELECT * FROM cis_course_catalog WHERE last_activity_date>='${DATE}';
 spool off;
 exit;
 EOF
-curl -sS "http://courseroad.mit.edu/dev/output.php"
+curl -sS "http://courseroad.mit.edu/dev/output.php${TEST_MODE}";
